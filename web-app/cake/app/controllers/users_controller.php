a8e1fc5dc25d4d79db78c00b9a40786ac1d3c39a
Sema Berkiten <semaberkiten@gmail.com>
2011-04-01 15:49:33 -0400

    user

--- web-app/cake/app/controllers/users_controller.php
+++ web-app/cake/app/controllers/users_controller.php
@@ -51,7 +51,8 @@ class UsersController extends AppController
     	//register a new user
     	if( !empty( $this->data ) )
     	{
-$this->data['User']['password_copy'] = $this->data['User']['password'];
+    		if (!empty($this->data['User']['password']))
+				$this->data['User']['password_copy'] = $this->data['User']['password'];
     		//sanitize the password
     		//$this->User->data = Sanitize::clean($this->data);
     		$this->User->create();
