7e2c0e89e70e2e2fd7ab4571f6530cbc869e6527
Henry Liu <henryliu@princeton.edu>
2011-04-01 13:47:02 -0400

    henry testing

--- phone-app/src/com/peoples/android/Peoples.java
+++ phone-app/src/com/peoples/android/Peoples.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class Peoples extends Activity {
     // Debugging
+	// TEST
     private static final String TAG = "PEOPLES";
     private static final boolean D = true;
     
