f8b1599f2630e754dd34c66d3300f25a5d503272
Diego <diego@dynamic-oit-vapornet-a-1081.princeton.edu>
2011-04-03 10:40:19 -0400

    Added calls, yet to be fully implemented to a few android.location classes we'll need to use for GPS

--- phone-app/src/com/peoples/android/processTest/LocationTestActivity.java
+++ phone-app/src/com/peoples/android/processTest/LocationTestActivity.java
@@ -1,7 +1,59 @@
 package com.peoples.android.processTest;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class LocationTestActivity extends Activity {
-
+	
+	
+	/** Called when the activity is first created. */
+	   @Override
+	   public void onCreate(Bundle savedInstanceState) {
+	       super.onCreate(savedInstanceState);
+	       
+	       Intent locationIntent = new Intent(this, LocationTestService.class);
+	       startService(locationIntent);
+	       
+	       LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	       
+	       LocationListener locListener = new LocationListener() {
+			
+			public void onStatusChanged(String provider, int status, Bundle extras) {
+				// TODO Auto-generated method stub
+				doSomethingWithLocation();
+			}
+			
+			public void onProviderEnabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onProviderDisabled(String provider) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			public void onLocationChanged(Location location) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+	       
+	       
+	       
+	       
+	       
+	       
+	       
+	       
+	       TextView tv = new TextView(this);
+	       tv.setText("Hello, Android");
+	       setContentView(tv);
+	   }
 }
