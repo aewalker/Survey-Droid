31eb5bf524f3c2f70348fa3745383b5951b900a9
Austin Walker <awalkerenator@gmail.com>
2011-04-05 02:56:31 -0400

    Added subjects validation rules in models/subjects.

--- web-app/cake/app/models/subject.php
+++ web-app/cake/app/models/subject.php
@@ -14,7 +14,34 @@ class Subject extends AppModel
 		'phone_num' => array
 		(
 			//change from 'us' if you want to use international phone numbers
-			'rule' => array('phone', null, 'us')
+			'rule' => array('phone', null, 'us'),
+			'message' => 'Please provide a phone number'
+		),
+		'first_name' => array
+		(
+			'minLength' => array
+			(
+				'rule' => array('minLength', 1),
+				'message' => 'Please provide a first name'
+			),
+			'maxLength' => array
+			(
+				'rule' => array('maxLength', 320),
+				'message' => 'First name cannot be longer than 320 characters'
+			)
+		),
+		'last_name' => array
+		(
+			'minLength' => array
+			(
+				'rule' => array('minLength', 1),
+				'message' => 'Please provide a last name'
+			),
+			'maxLength' => array
+			(
+				'rule' => array('maxLength', 320),
+				'message' => 'Last name cannot be longer than 320 characters'
+			)
 		)
 	);
 }
