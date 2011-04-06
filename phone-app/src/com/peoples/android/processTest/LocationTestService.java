dec14fc64baf6f17f39b5d5f71af52f54535e957
Henry Liu <henryliu@princeton.edu>
2011-04-06 15:29:32 -0400

    Sample GPS added. prints location and time roughly every 15 seconds

--- phone-app/src/com/peoples/android/processTest/LocationTestService.java
+++ phone-app/src/com/peoples/android/processTest/LocationTestService.java
@@ -6,6 +6,17 @@ package com.peoples.android.processTest;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.IBinder;
+import android.app.Activity;
+import android.content.Context;
+import android.location.GpsStatus.Listener;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * @author diego
@@ -23,15 +34,53 @@ public class LocationTestService extends IntentService {
 
 	@Override
 	protected void onHandleIntent(Intent intent) {
-		// TODO Auto-generated method stub
-		
-		//ok find out how to get location here 
-		
-		//find out how to send location here
-		
-		//then close anything that needs to be closed.
-		
+		LocationManager mlocManager =(LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		LocationListener mlocListener = new MyLocationListener();
+		mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
 	}
+	
+	/* Class My Location Listener */
+
+	public class MyLocationListener implements LocationListener
+	{
+
+		@Override
+		public void onLocationChanged(Location loc)
+		{
+			loc.getLatitude();
+			loc.getLongitude();
+			String Text = "My current location is: " +
+			"Latitud = " + loc.getLatitude() +
+			"Longitud = " + loc.getLongitude();
+			Toast.makeText( getApplicationContext(),
+			Text,
+			Toast.LENGTH_SHORT).show();
+		}
+	
+	
+		@Override
+		public void onProviderDisabled(String provider)
+		{
+			Toast.makeText( getApplicationContext(),
+			"Gps Disabled",
+			Toast.LENGTH_SHORT ).show();
+		}
+	
+	
+		@Override
+		public void onProviderEnabled(String provider)
+		{
+			Toast.makeText( getApplicationContext(),
+			"Gps Enabled",
+			Toast.LENGTH_SHORT).show();
+		}
+	
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras)
+		{
+		}
+
+	}/* End of Class MyLocationListener */
 
 	
 	
