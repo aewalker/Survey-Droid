9af1acaca6bd848cde8f7e7367d430572abe5921
Diego <diego@dynamic-oit-vapornet-a-1081.princeton.edu>
2011-04-08 07:49:01 -0400

    working on a survey u.i.

--- phone-app/src/com/peoples/android/Peoples.java
+++ phone-app/src/com/peoples/android/Peoples.java
@@ -2,7 +2,13 @@ package com.peoples.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Peoples extends Activity {	
     // Debugging
@@ -16,8 +22,34 @@ public class Peoples extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("hi");
-        setContentView(tv);
+
+        if(D) Log.e(TAG, "+++ ON CREATE +++");
+        
+        // Set up the window layout
+        setContentView(R.layout.shortresponseview);
+        
+        
+        final EditText edittext = (EditText) findViewById(R.id.editText1);
+        edittext.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                    (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                  // Perform action on key press
+                  Toast.makeText(Peoples.this, edittext.getText(), Toast.LENGTH_SHORT).show();
+                  return true;
+                }
+                return false;
+            }
+
+        });
+        
+//        this.startService(new Intent(this, BootService.class));
+        
+        
+// Testing UI above       
+//        this.startService(new Intent(this, CallLogService.class));
+
+
     }
 }
---
+++ phone-app/src/com/peoples/android/activities/SurveyActivity.java
@@ -0,0 +1,25 @@
+package com.peoples.android.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SurveyActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		
+		
+		super.onCreate(savedInstanceState);
+		
+		
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+		
+	}
+
+}
