8835c0636d9573307c2467f327c9f5cf32034372
Diego <diego@dynamic-oit-pu-dormnet-bb-b-87.princeton.edu>
2011-04-15 22:27:17 -0400

    nice view of how a few of the android.location classes interact

--- phone-app/src/com/peoples/android/processTest/LocationTestActivity.java
+++ phone-app/src/com/peoples/android/processTest/LocationTestActivity.java
@@ -42,79 +42,15 @@ public class LocationTestActivity extends Activity {
 
 	        });
 	       
-	       doTheRest();
+	       
 	       
 	       
 	   }
 
 
-	private void doTheRest() {
-		/*private void doTheRest(final TextView tv) {*/
-		
-//		TODO: Will make this an activity, and will move GPS gathering to a Service
-//		Intent locationIntent = new Intent(this, LocationTestService.class);
-//		startService(locationIntent);
-
-		//get a location manager from the system
-		LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-		// create a Listener interface that will handle the GPS location update
-		LocationListener locListener = new MyLocationListener(); 
-		
-		//subscribe our Listener to the locManager
-		/*locManager.addGpsStatusListener(gpsLocListener);*/
-		locManager.requestLocationUpdates("gps", 0, 0, locListener);
-		
-	}
-	
-	public class MyLocationListener implements LocationListener
-	{
-		private int i = 0;
-
-		@Override
-		public void onLocationChanged(Location loc)
-		{
-			i++;
-			if (i == 1)
-			{
-				Date date = new Date();
-				loc.getLatitude();
-				loc.getLongitude();
-				String Text = "My current location is: \n" +
-				"Latitude = " + loc.getLatitude() +
-				"\nLongitude = " + loc.getLongitude() + 
-				"\n" + date.toString();
-				Toast.makeText( getApplicationContext(),
-								Text,
-								Toast.LENGTH_SHORT).show();
-				i = 0;
-			}
-		}
 	
 	
-		@Override
-		public void onProviderDisabled(String provider)
-		{
-			Toast.makeText( getApplicationContext(),
-			"Gps Disabled",
-			Toast.LENGTH_SHORT ).show();
-		}
 	
-	
-		@Override
-		public void onProviderEnabled(String provider)
-		{
-			Toast.makeText( getApplicationContext(),
-			"Gps Enabled",
-			Toast.LENGTH_SHORT).show();
-		}
-	
-		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras)
-		{
-		}
-
-	}/* End of Class MyLocationListener */
 	
 	
 }
--- phone-app/src/com/peoples/android/services/GPSLocationService.java
+++ phone-app/src/com/peoples/android/services/GPSLocationService.java
@@ -1,20 +1,33 @@
 package com.peoples.android.services;
 
+import java.util.Date;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Criteria;
+import android.location.GpsSatellite;
+import android.location.GpsStatus.Listener;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.util.Log;
+import android.widget.Toast;
 
 public class GPSLocationService extends Service {
 	
 	
 	private static final String TAG = "GPSLocationService";
     private static final boolean D = true;
+    
+    private static Location location;
 
 	/**
 	 * 
@@ -30,7 +43,6 @@ public class GPSLocationService extends Service {
 
 		
 		AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-		
 		Intent gpsServiceIntent = new Intent(getApplicationContext(), GPSLocationService.class);
 		
 		//TODO: probably not the best way to getApplicaitonContext()
@@ -47,7 +59,59 @@ public class GPSLocationService extends Service {
 									30*1000,
 									pendingGPS);
 		
+		if(D) Log.e(TAG, "added alarm item, now signing up with LocationManager");
+		
+		//get a location manager from the system
+		final LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		
+		
+		if(D) Log.e(TAG, "Best provider:");
+			String provider = locManager.getBestProvider(new Criteria(), true);
+			Log.e(TAG, provider);
+			
+			
+		/**
+		 * 
+		 * this is neat, move phone from indoors to somewhere it can get GPS signal
+		 * you'll see it increase the number of satellites it can pick up and
+		 * eventually fixate call status 3, when a fix is obtained.
+		 * 	
+		 */
+		if(D) Log.e(TAG, "Signing up for GpsStatus:");
+			locManager.addGpsStatusListener( new GpsStatus.Listener() {
+					
+					public void onGpsStatusChanged(int event) {
+					
+						GpsStatus status = locManager.getGpsStatus(null);
+						
+						switch(event){
+							case 1: Log.e(TAG, "GPS Status: started"); break;
+							case 2: Log.e(TAG, "GPS Status: stopped"); break;
+							case 3: Log.e(TAG, "GPS Status: first fix at time:");
+									Log.e(TAG, ""+status.getTimeToFirstFix());
+									Location loc = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+									Log.e(TAG, "Location object is:");
+									Log.e(TAG, loc.toString() + "\n");
+									break;
+							case 4: Log.e(TAG, "GPS Status: max satellites:");
+									Log.e(TAG, ""+status.getMaxSatellites());
+									Log.e(TAG, "GPS Status: but actual satellites:");
+									
+									int i = 0;
+									for(GpsSatellite s : status.getSatellites() ){
+										i++;
+										Log.e(TAG, s.toString());
+									}
+									Log.e(TAG, "There were "+i+" satellites in total");
+									break;
+						}
+						Log.e(TAG, "GPS Status: " + event );					
+				}
+			});
 	}
+	
+	
+	
 	
 	/**
 	 * 
@@ -56,24 +120,99 @@ public class GPSLocationService extends Service {
 	 * 
 	 */
 	@Override
-	public ComponentName startService(Intent service) {
-		
-		if(D) Log.e(TAG, "+++GPSLocationService.startService()+++");
-		return super.startService(service);
-		
-	}
-	
-	
-	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		if(D) Log.e(TAG, "+++GPSLocationService.onStartCommand()+++");
+		
+		Context context = getApplicationContext();
+		CharSequence text = "onStartCommand GPSLocation Service";
+		int duration = Toast.LENGTH_LONG;
+
+		Toast toast = Toast.makeText(context, text, duration);
+		toast.show();
+				
 		return super.onStartCommand(intent, flags, startId);
 	}
 	
+	public class MyLocationListener implements LocationListener
+	{
+		public MyLocationListener() {
+			super();
+		}
+		
+		@Override
+		public void onLocationChanged(Location loc)
+		{
+			
+			Date date = new Date();
+			loc.getLatitude();
+			loc.getLongitude();
+			String Text = "My current location is: \n" +
+			"Latitude = " + loc.getLatitude() +
+			"\nLongitude = " + loc.getLongitude() + 
+			"\n" + date.toString();
+			Toast.makeText( getApplicationContext(),
+					Text,
+					Toast.LENGTH_SHORT).show();
+
+		}
+	
+	
+		@Override
+		public void onProviderDisabled(String provider)
+		{
+			Toast.makeText( getApplicationContext(),
+			"Gps Disabled",
+			Toast.LENGTH_SHORT ).show();
+		}
+	
+	
+		@Override
+		public void onProviderEnabled(String provider)
+		{
+			Toast.makeText( getApplicationContext(),
+			"Gps Enabled",
+			Toast.LENGTH_SHORT).show();
+		}
+	
+		@Override
+		public void onStatusChanged(String provider, int status, Bundle extras)
+		{
+			if(D) Log.e(TAG, "+++GPS onStatusChanged+++");
+		}
+
+	}/* End of Class MyLocationListener */
+	
+	
+	private void initializeGPS(LocationManager locManager, PendingIntent pendingGPS) {
+		/*private void doTheRest(final TextView tv) {*/
+		
+//		TODO: Will make this an activity, and will move GPS gathering to a Service
+//		Intent locationIntent = new Intent(this, LocationTestService.class);
+//		startService(locationIntent);
+
+		
+		// create a Listener interface that will handle the GPS location update
+		LocationListener locListener = new MyLocationListener(); 
+		
+		//subscribe our Listener to the locManager
+		/*locManager.addGpsStatusListener(gpsLocListener);*/
+		locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 7*1000, 0, pendingGPS);
+		
+		String location = "null";
+		if(locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) != null)
+			location = locManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).toString();
+		
+		if(D) Log.e(TAG, location);
+		
+	}
+
 	@Override
 	public IBinder onBind(Intent arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	
+	
+	
 }
