1c259a10d2eeea710495e226e2e5610746a2f4fa
Henry Liu <henryliu@princeton.edu>
2011-04-18 16:18:44 -0400

    GUI Stuffs

---
+++ phone-app/src/com/peoples/android/activities/ConfirmSubmissionSurvey.java
@@ -0,0 +1,26 @@
+package com.peoples.android.activities;
+
+import src.com.peoples.model.Question;
+import src.com.peoples.model.Survey;
+
+import com.peoples.android.Peoples;
+import com.peoples.android.R;
+import com.peoples.android.processTest.LocationTestActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ConfirmSubmissionSurvey extends Activity {
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+
+}
