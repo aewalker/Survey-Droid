cbdf95d6f1e8c9c8837d4831ebcb58c8aaf92728
Tony Xiao <tonyx.ca@gmail.com>
2011-04-19 05:48:22 -0400

    updated the webclient again

--- phone-app/src/com/peoples/android/server/WebClient.java
+++ phone-app/src/com/peoples/android/server/WebClient.java
@@ -8,13 +8,18 @@ package com.peoples.android.server;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 /**
  * Helper methods to simplify talking with and parsing responses from a
@@ -22,7 +27,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
  */
 public class WebClient {
     private static final String TAG = "WebClient";
-
+    private static final int HTTP_STATUS_OK = 200;
     /**
      * Thrown when there were problems contacting the remote API server, either
      * because of a network error, or the server returned a bad status code.
@@ -48,7 +53,7 @@ public class WebClient {
      * @throws ApiException If any connection or server error occurs.
      */
     protected static synchronized String getUrlContent(String url) throws ApiException {
-        final int HTTP_STATUS_OK = 200;
+        
         byte[] sBuffer = new byte[512];
         
         // Create client and set our specific user-agent string
@@ -85,7 +90,21 @@ public class WebClient {
         }
     }
     
-    protected static synchronized boolean postToUrl() {
-        return true;
+    protected static synchronized boolean postToUrl(String url, String name, String value) {
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+            List nameValuePairs = new ArrayList(1);
+            nameValuePairs.add(new BasicNameValuePair(name, value));
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httpPost);
+            
+            StatusLine status = response.getStatusLine();
+            if (status.getStatusCode() == HTTP_STATUS_OK)
+                return true;
+        } catch (Exception e) { }
+        return false;
     }
 }
