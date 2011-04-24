6966869461e87054e70999a2cacdd9e383a69711
Austin Walker <awalkerenator@gmail.com>
2011-04-24 17:35:44 -0400

    Updated survey model and UI

--- phone-app/src/com/peoples/android/model/Branch.java
+++ phone-app/src/com/peoples/android/model/Branch.java
@@ -1,30 +1,69 @@
+/*---------------------------------------------------------------------------*
+ * Branch.java                                                               *
+ *                                                                           *
+ * Model for a survey branch. Can be evaluated to true/false to determine    *
+ * what question to display next.  Contains a set of conditions.             *
+ *---------------------------------------------------------------------------*/
 package com.peoples.android.model;
 
+import java.util.Collection;
 
 /**
+ * Model for a survey branch.  Based on the SQL:
+ * 
  * CREATE TABLE branches (
-	id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	prev_q INT UNSIGNED NOT NULL, //foreign keys//
-	next_q INT UNSIGNED NOT NULL)
+ *  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ *  prev_q INT UNSIGNED NOT NULL, //foreign keys//
+ *  next_q INT UNSIGNED NOT NULL)
  *
- * @author Diego
- *
+ * @author Diego Vargas
+ * @author Austin Walker
  **/
-public class Branch {
-
-	//id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	private int id;
-
-	//prev_q INT UNSIGNED NOT NULL, /*foreign keys*/
-	private Question prev_q;
-
-	//next_q INT UNSIGNED NOT NULL);
+public class Branch
+{
+	//The next question to go to if this branch is true.  Can be null.
 	private Question next_q;
+	
+	//Set of Conditions that belong to this branch; starts empty
+	private Collection<Condition> conditions;
+	
+	/*-----------------------------------------------------------------------*/
 
-	public Branch (Question p, Question n, int id) {
-		this.prev_q = p;
-		this.next_q = n;
-		this.id = id;
+	/**
+	 * Create a new Branch with no Conditions.
+	 * 
+	 * @param q - Question this Branch should point to
+	 * @param c - Conditions for this Branch
+	 */
+	public Branch(Question q, Collection<Condition> c)
+	{
+		next_q = q;
+		conditions = c;
 	}
-
+	
+	/**
+	 * Evaluate this Branch.
+	 * 
+	 * @return true or false
+	 */
+	public boolean eval()
+	{
+		boolean status = true;
+		for (Condition c : conditions)
+		{
+			if (c.eval() == false)
+				status = false;
+		}
+		return status;
+	}
+	
+	/**
+	 * Get the Question this Branch points to
+	 * 
+	 * @return Question this Branch points to
+	 */
+	public Question nextQuestion()
+	{
+		return next_q;
+	}
 }
