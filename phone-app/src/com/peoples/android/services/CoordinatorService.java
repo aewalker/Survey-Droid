540df6e2ed4576b5e1c56934da8d4b2f367bccd2
Diego <egodiego@gmail.com>
2011-05-03 15:32:56 -0400

    SurveyScheduler now on timer

--- phone-app/src/com/peoples/android/services/CoordinatorService.java
+++ phone-app/src/com/peoples/android/services/CoordinatorService.java
@@ -40,6 +40,11 @@ public class CoordinatorService extends IntentService {
 		Log.d(TAG, "Fetching surveys");
         Pull.syncWithWeb(this);
 		
+        //will need one of these to schedule services
+        AlarmManager alarmManager =
+        	(AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        
+        
 		//TODO: iterate through survey table and schedule surveys
         //need to somehow find row of unanswered survey
         
@@ -47,10 +52,17 @@ public class CoordinatorService extends IntentService {
 		//for now, let's try scheduling a survey every minute?
 		//TODO: figure out proper flag!!!
         
-        //TODO: have alarmManager run SurveyScheduler at specified
-        //interval
+        //TODO: have alarmManager run SurveyScheduler at specified interval
+        Intent surveySchedulerIntent = new Intent(this, SurveyScheduler.class);
+        PendingIntent pendingScheduler = PendingIntent.getService(this, 0,
+        		surveySchedulerIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        alarmManager.setRepeating(
+        					AlarmManager.ELAPSED_REALTIME_WAKEUP,
+        					SystemClock.elapsedRealtime(),
+        					60*1000, pendingScheduler);
         
-		AlarmManager alarmManager 	= (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        //this is setting a recurring survey
+		
 		Intent surveyIntent			= new Intent(this, Peoples.class);
 		
 		PendingIntent pendingSurvey = PendingIntent.getActivity(this, 0, surveyIntent,
